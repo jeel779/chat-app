510cb197c3cf5db21a6d218458d5e8a3e66f824f
jeel <jeelpatel779@gmail.com>
2026-07-07 16:45:26 +0530

    change in packge.json

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,10 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import { app } from "./app.js";
 import { createServer } from "http";
 import { initializeSocket } from "./socket/socket.js";
+dotenv.config({
+    path: './.env'
+});
 const server = createServer(app);
 initializeSocket(server);
 server.listen(process.env.PORT || 8000, () => {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
-import "dotenv/config";
+import dotenv from "dotenv"
 import { app } from "./app.js";
 import { createServer } from "http";
 import { initializeSocket } from "./socket/socket.js";
 
+dotenv.config({
+    path: './.env'
+})
 const server = createServer(app);
 initializeSocket(server);
 
